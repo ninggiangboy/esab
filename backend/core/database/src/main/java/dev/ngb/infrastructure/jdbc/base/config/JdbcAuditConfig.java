1911a7143985cc1df1166b79126d1bdf776f11f2
ninggiangboy <ninggiangboy@gmail.com>
2026-03-14 13:12:09 +0700

    feat: enable JDBC auditing with configuration class

---
+++ backend/core/database/src/main/java/dev/ngb/infrastructure/jdbc/base/config/JdbcAuditConfig.java
@@ -0,0 +1,9 @@
+package dev.ngb.infrastructure.jdbc.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+
+@Configuration
+@EnableJdbcAuditing
+public class JdbcAuditConfig {
+}
