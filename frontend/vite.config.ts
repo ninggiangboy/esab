50aec50467ff5a51b9b4414dc8a09da81d1b03d3
ninggiangboy <ninggiangboy@gmail.com>
2026-04-02 22:00:31 +0700

    refactor: replace Vue beta dependencies, add auto-import and component declarations, and update TypeScript configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,22 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      imports: ['vue'],
+      dts: './auto-imports.d.ts',
+    }),
+    Components({
+      dirs: ['src/components/ui'],
+      dts: './components.d.ts',
+    }),
     tailwindcss(),
   ],
   resolve: {
